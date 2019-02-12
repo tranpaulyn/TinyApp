7f412323c236da5aace4c920bc9d79534ee3b6bd
Paulyn <paulyntran@live.ca>
2019-02-12 21:24:08 +0000

    Created URLS Index page, with link to shorten URLS

--- express_server.js
+++ express_server.js
@@ -16,12 +16,13 @@ app.get("/", (req, res) => {
 
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b> World</b></body></html>\n");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-})
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars)
+});
