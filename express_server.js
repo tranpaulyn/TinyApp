e090b8af262fae2e5c6d8d42372466a22fd66dd4
Paulyn <paulyntran@live.ca>
2019-02-13 00:14:53 +0000

    Added generateRandomString function, bodyParser and post ok

--- express_server.js
+++ express_server.js
@@ -10,6 +10,9 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -27,8 +30,18 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars)
 });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, 
         longURL: urlDatabase[req.params.shortURL]};
     res.render("urls_show", templateVars);
 });
+
