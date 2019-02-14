8e69fa07fb002456cbbc24d79925994ed3b12294
Paulyn <paulyntran@live.ca>
2019-02-14 05:31:28 +0000

    merged branch to master and lint adjustments

--- express_server.js
+++ express_server.js
@@ -1,28 +1,28 @@
-var express = require("express");
+var express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 var PORT = 8080; // default port 8080
 
 
 // this tells the Express app to use EJS as its templating engine
-app.set("view engine", "ejs"); 
+app.set('view engine', 'ejs'); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b> World</b></body></html>\n");
+app.get('/hello', (req, res) => {
+    res.send('<html><body>Hello <b> World</b></body></html>\n');
 });
 
 app.listen(PORT, () => {
@@ -30,24 +30,24 @@ app.listen(PORT, () => {
 });
 
 //Home Page -- lists all URLS & login/logout
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
     let templateVars = {
         urls: urlDatabase,
         username: req.cookies.username };
-    res.render("urls_index", templateVars)
+    res.render('urls_index', templateVars)
 });
 
 //Create New Tiny URL Page
-app.get("/urls/new", (req, res) => {
+app.get('/urls/new', (req, res) => {
     let templateVars = {
         username: req.cookies.username };
-    res.render("urls_new", templateVars)
+    res.render('urls_new', templateVars)
 });
 
 //Generate random string to create new tiny URL
 function generateRandomString() {
-    let text = "";
-    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   
     for (var i = 0; i < 6; i++)
       text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -56,32 +56,32 @@ function generateRandomString() {
 }
 
 // Create New Tiny URL & Add to URL Datbase
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
     const newShortURL = generateRandomString();
     urlDatabase[newShortURL] = req.body.longURL;
     console.log(urlDatabase);
-    res.redirect("/urls/" + newShortURL)
+    res.redirect('/urls/' + newShortURL)
 });
 
 //Cookie Business -- create a cookie
 app.post('/login', (req, res) => {
-    res.cookie("username", req.body.username)
+    res.cookie('username', req.body.username)
     res.redirect('/urls');
 });
 
 // Clear Cookie & Logout
 app.post('/logout', (req, res) => {
-    res.clearCookie("username")
+    res.clearCookie('username')
     res.redirect('/urls');
 });
 
 
 // Short URL Page - can edit/update on this page
-app.get("/urls/:shortURL", (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, 
         longURL: urlDatabase[req.params.shortURL],
         username: req.cookies.username };
-    res.render("urls_show", templateVars);
+    res.render('urls_show', templateVars);
 });
 
 // Link short url to long URL
